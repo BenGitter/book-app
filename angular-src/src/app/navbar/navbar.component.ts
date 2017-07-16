a47ecd15233815eaf88f78f7c2cd6710e9ac52bc
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-16 22:23:06 +0200

    Close navbar dropdown when navigated

--- angular-src/src/app/navbar/navbar.component.ts
+++ angular-src/src/app/navbar/navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
+  navigationSub:Subscription;
   collapseOpen:boolean = false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    this.navigationSub = this.router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe(data => {
+        this.collapseOpen = false;
+      });
+  }
+
+  ngOnDestroy(){
+    this.navigationSub.unsubscribe();
   }
 
 }
